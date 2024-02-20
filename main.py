6dd0935cb81fa35f723f687ca0ca691c02aaabc5
LMiguel14 <159493106+LMiguel14@users.noreply.github.com>
2024-02-20 10:20:54 -0500

    Final

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 class Auto():
   cantidadCreados = 0
+  
   def __init__(self, modelo, precio, asientos, marca, motor, registro):
-    self.modelo = modelo
+    self.modelo = modelo                                                              
     self.precio = precio
     self.asientos = asientos
     self.marca = marca
@@ -14,32 +15,43 @@ class Auto():
       if isinstance(i, Asiento) == True:
         asientos += 1
     return asientos
-     
-    
+  
   def verificarIntegridad(self):
-    pass
-        
+    registrosAsientos = []
+    registros= [self.registro, self.motor.registro]
+    for i in self.asientos:
+      if isinstance(i, Asiento) == True:
+        registrosAsientos.append(i.registro)
+    if registrosAsientos == registros :
+      print("Auto original")
+    else:
+      print("Las piezas no  son originales")  
+
 
 class Motor():
+
   def __init__(self, cilindros, tipo, registro):
     self.cilindros = cilindros
     self.tipo = tipo
     self.registro = registro
+
   def cambiarRegistro(self, numero):
     self.registro = numero
+
   def asignarTipo(self, nuevoTipo):
     if (nuevoTipo == "gasolina") or (nuevoTipo== "electrico"):
       self.tipo= nuevoTipo
 
 
 class Asiento():
+
   def __init__ (self, color , precio , registro):
     self.color =    color
     self.precio =   precio
     self.registro = registro
+
   def cambiarColor(self,  nuevo_color):
     colores = ["verde", "amarillo", "blanco", "negro" , "rojo",]
     if nuevo_color in colores:
       self.color = nuevo_color
-
 
